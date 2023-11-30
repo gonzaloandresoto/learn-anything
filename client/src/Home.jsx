09abf0fd322a36db56ba6cd175ec2518aefee3a2
gonzaloandresoto <gonzandresoto@gmail.com>
2023-11-30 18:35:40 -0500

    nav bar

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -27,6 +27,20 @@ function Home() {
   return (
     <div className='w-screen h-screen flex flex-col grow items-center bg-white'>
       <div className='w-2/3 flex flex-col gap-12 py-16'>
+        <div className='flex justify-center'>
+          <div className='flex items-center gap-4 border border-secondary-grey rounded-md'>
+            <button
+              className={`w-[104px] py-1 text-sm text-white font-medium bg-primary-indigo rounded-md`}
+            >
+              Home
+            </button>
+            <button
+              className={`w-[104px] py-1 text-sm text-primary-black font-medium bg-white rounded-md`}
+            >
+              Me
+            </button>
+          </div>
+        </div>
         <div className='flex flex-col gap-4'>
           <p className='text-lg text-primary-black font-semibold'>
             I want to learn about...
